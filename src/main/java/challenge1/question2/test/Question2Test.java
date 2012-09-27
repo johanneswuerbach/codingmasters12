b3599214d9992f7cbd526c0a80b4ead234225bc7
Fabian Michels <f.michels@hotmail.de>
2012-09-27 19:54:20 +0200

    Test classes

---
+++ src/main/java/challenge1/question2/test/Question2Test.java
@@ -0,0 +1,29 @@
+package challenge1.question2.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import challenge1.question2.Question2;
+
+public class Question2Test {
+
+	private Question2 question2 = null;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.question2 = new Question2();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
