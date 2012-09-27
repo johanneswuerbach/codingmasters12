759e5a910108b17fc9adb4ddb03441ac8e8e9a2a
Johannes Würbach <johannes.wuerbach@googlemail.com>
2012-09-27 20:58:01 +0200

    Removed Main, Iterator works with multiple paths

--- src/main/java/challenge1/Parser.java
+++ src/main/java/challenge1/Parser.java
@@ -3,17 +3,23 @@ package challenge1;
 
 public class Parser {
 
-	private final String _directory;
+	private final String[] _directories;
+	
+	public Parser(String[] directories) {
+		_directories = directories;
+	}
 
 	public Parser(String directory) {
-		_directory = directory;
+		_directories = new String[]{directory};
 	}
+	
+	
 
 	public TweetIterator tweetIterator() {
 		return new TweetIterator(rawIterator());
 	}
 	
 	public RawIterator rawIterator() {
-		return new RawIterator(_directory);
+		return new RawIterator(_directories);
 	}
 }
