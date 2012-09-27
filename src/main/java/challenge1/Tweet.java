886820d08f0da3b0b593a29fe60e1c2fecfdc03f
Johannes Würbach <johannes.wuerbach@googlemail.com>
2012-09-27 18:26:02 +0200

    Tweet iterator

--- src/main/java/challenge1/Tweet.java
+++ src/main/java/challenge1/Tweet.java
@@ -1,5 +1,17 @@
 package challenge1;
 
+import org.json.simple.JSONObject;
+
 public class Tweet {
+	
+	private JSONObject _object;
+
+	public Tweet(JSONObject object) {
+		_object = object;
+	}
+	
+	public String getText() {
+		return (String) _object.get("text");
+	}
 
 }
