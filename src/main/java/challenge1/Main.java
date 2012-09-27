d9c955d9ce49c2667959c5babbcdf737cd13fd91
Taig <taig@AiR>
2012-09-27 17:39:41 +0200

    Switched from gson to json-simple.

--- src/main/java/challenge1/Main.java
+++ src/main/java/challenge1/Main.java
@@ -1,13 +1,20 @@
 package challenge1;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Test.");
+		Object parse = JSONValue.parse( "{\"1\":{\"2\":{\"3\":{\"4\":[5,{\"6\":7}]}}}}" );
+		JSONObject test = (JSONObject) parse;
+		
+		System.out.println(test.get( "1" ));
 	}
 
 }
