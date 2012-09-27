c9c906f2bb06b25eb86e4d9a66e3e2ee74db1f36
Johannes Würbach <johannes.wuerbach@googlemail.com>
2012-09-27 16:41:23 +0200

    First commit

---
+++ challenge1/main.java
@@ -0,0 +1,13 @@
+package challenge1;
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sdasd");
+	}
+
+}
